7a73aed5383900e4bffde98e90200446d408ddcd
OliveiraHenrique <oliveirahenrique70@gmail.com>
2022-12-07 09:50:41 -0300

    front-end improv

--- app.R
+++ app.R
@@ -48,48 +48,127 @@ app_catalog_tbl <- bind_rows(app1, app2, app3, .id = "id")
 navbar_page_with_inputs <- function(..., inputs) {
     navbar <- navbarPage(...)
     form <- tags$form(class = "navbar-form navbar-right", inputs)
-    navbar[[4]][[1]][[1]]$children[[1]]$children[[2]] <- htmltools::tagAppendChild(
-        navbar[[4]][[1]][[1]]$children[[1]]$children[[2]], form)
+    navbar[[4]][[1]][[1]]$children[[1]]$children[[2]] <-
+        htmltools::tagAppendChild(navbar[[4]][[1]][[1]]$children[[1]]$children[[2]], form)
     navbar
 }
 
 # UI ----
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-    
     # Header of WebPage
-    tagList(
-        tags$head(HTML("<title>Henrique Oliveira Apps</title>"))
+    tagList(tags$head(
+        HTML("<title>Henrique Oliveira Apps</title>")
+    )),
+    
+    
+    tags$style(
+        HTML(
+            '.navbar-nav > li > a, .navbar-brand {
+                   padding-top:5px !important;
+                   padding-bottom:0 !important;
+                   height: 35px;
+                 }
+                 .navbar {min-height:60px !important;}'
+        )
     ),
+    
     style = "padding:0px",
     
     themeSelector(),
-
+    
     # Navbar page
     navbar_page_with_inputs(
-        title = "Test",
+        title = div(img(src = "logo.gif",
+                        width = 50),
+                    "Henrique Oliveira Apps"),
+        
         collapsible = TRUE,
-
+        
         # Search box
         inputs = div(
-            textInput(inputId = "search_box",
-                      label = NULL,
-                      width = 200),
+            textInput(
+                inputId = "search_box",
+                label = NULL,
+                width = 200
+            ),
             actionButton(inputId = "search_button",
                          label = "Submit"),
             actionButton(inputId = "clear_button",
                          label = "Clear")
         ),
-
+        
         # Tabs
-        tabPanel(title = "Library")
+        tabPanel(
+            "Library",
+            
+            # 1.3.1 Tag Filters ----
+            div(
+                class = "container", 
+                id = "tag-filters", 
+                radioGroupButtons(
+                    inputId = "input_tags", 
+                    choices = c("Choice A", "Choice B", " Choice C", "Choice D"), 
+                    justified = TRUE, 
+                    status = "default"
+                )
+            ),
+            
+            # 1.3.2 App Library ----
+            div(
+                class = "container", 
+                id = "app-library",
+                uiOutput(outputId = "output_cards")
+            )
+        )
+        
     )
 )
 
 # SERVER ----
 server <- function(session, input, output) {
-    
+    # Add App Cards
+    output$output_cards <- renderUI({
+        div(class = "container",
+            div(
+                class = "row",
+                
+                # First Card
+                div(
+                    class = "col-sm-4",
+                    style = "display:flex",
+                    div(
+                        class = "panel panel-default",
+                        div(class = "panel-heading",
+                            span(class = "label label-info",
+                                 "AWS")),
+                        div(
+                            class = "panel-body",
+                            style = "padding:20px;",
+                            
+                            img(class = "imh img-thumbnail",
+                                src = "stock_analyzer.jpg"),
+                            
+                            br(),
+                            br(),
+                            h4("Stock Analizer"),
+                            br(),
+                            tags$small("MongoDb Atlas")
+                        ),
+                        p(app_catalog_tbl$description[[1]],
+                          style = "padding:20px;"),
+                        a(type = "button",
+                          class = "btn btn-primary",
+                          target = "_blank",
+                          href = str_c("/", app_catalog_tbl$sub_directory[[1]]),
+                          "Open",
+                          style = "margin:20px;"
+                        )
+                    )
+                )
+            ))
+    })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
