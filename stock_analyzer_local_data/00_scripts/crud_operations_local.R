17c8b8c757b8038bce99b29f605d13c9616b0b87
OliveiraHenrique <oliveirahenrique70@gmail.com>
2022-12-29 11:24:54 -0300

    fix error when update database

--- stock_analyzer_local_data/00_scripts/crud_operations_local.R
+++ stock_analyzer_local_data/00_scripts/crud_operations_local.R
@@ -5,5 +5,5 @@ function() {
 update_and_write_user_base <-
 function(user_name, column_name, assign_input) {
     user_base_tbl[user_base_tbl$user == user_name, ][[column_name]] <<- assign_input
-    write_rds(user_base_tbl, path = "00_data_local/user_base_tbl.rds")
+    write_rds(user_base_tbl, file = "00_data_local/user_base_tbl.rds")
 }
